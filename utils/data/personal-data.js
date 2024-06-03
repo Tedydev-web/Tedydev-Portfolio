db2a6f371232c7f627793dbd2e40a092d9a47ed9
Tedydev <Hieudat2310.bh@gmail.com>
2024-06-03 11:11:45 +0700

    update link resume

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -6,8 +6,7 @@ export const personalData = {
     "My name is TEDY DEV. I am an active, creative and enthusiastic programmer in everyday life. I am a fast learner with a self-taught attitude. I love learning and exploring new technologies and am passionate about problem solving. I love almost all web application development stacks and love making the web more open to the world. My core skills are based on C# and I love doing almost everything in C#. I am open to any type of job opportunity that suits my skills and interests.",
   email: "hieudat2310.bh@gmail.com",
   phone: "(+84) 0869685621",
-  address:
-    "Pham Van Thuan, Bien Hoa - Dong Nai, Vietnam.",
+  address: "Pham Van Thuan, Bien Hoa - Dong Nai, Vietnam.",
   github: "https://github.com/Tedydev-web",
   facebook: "https://www.facebook.com/profile.php?id=100065890170990",
   instagram: "https://www.instagram.com/hieuda_t/",
@@ -17,5 +16,5 @@ export const personalData = {
   gmail: "hieudat2310.bh@gmail.com",
   devUsername: "Tedydev",
   resume:
-    "https://qph6-my.sharepoint.com/:b:/g/personal/hieudat_qph6_onmicrosoft_com/EVFHmn9qz61PjP7fW1KyLN8BViGqToRB6HII6FAG-ve6zA?e=NGFrdO",
+    "https://qph6-my.sharepoint.com/:b:/g/personal/hieudat_qph6_onmicrosoft_com/EbNlbCJ_B8lHuwTYCW7yLnQB0RUzXQ_JVr-ip0cASjwIlg?e=ycZKTx",
 };
