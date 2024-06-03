c8f50f7b226b4d36d40d64ec8a92929a20199d90
Tedydev <Hieudat2310.bh@gmail.com>
2024-06-03 11:35:16 +0700

    lasted update

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "TEDY DEV",
-  profile: "/profile.png",
+  profile: "/profile.jpg",
   designation: "Developer",
   description:
     "My name is TEDY DEV. I am an active, creative and enthusiastic programmer in everyday life. I am a fast learner with a self-taught attitude. I love learning and exploring new technologies and am passionate about problem solving. I love almost all web application development stacks and love making the web more open to the world. My core skills are based on C# and I love doing almost everything in C#. I am open to any type of job opportunity that suits my skills and interests.",
@@ -16,5 +16,5 @@ export const personalData = {
   gmail: "hieudat2310.bh@gmail.com",
   devUsername: "Tedydev",
   resume:
-    "https://qph6-my.sharepoint.com/:b:/g/personal/hieudat_qph6_onmicrosoft_com/EbNlbCJ_B8lHuwTYCW7yLnQB0RUzXQ_JVr-ip0cASjwIlg?e=ycZKTx",
+    "https://qph6-my.sharepoint.com/personal/hieudat_qph6_onmicrosoft_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fhieudat%5Fqph6%5Fonmicrosoft%5Fcom%2FDocuments%2FWorkplace%2FDev%2FResume%2FResume%20of%20Tedy%20Dev%20%28Full%2DStack%20developer%29%2Epdf&parent=%2Fpersonal%2Fhieudat%5Fqph6%5Fonmicrosoft%5Fcom%2FDocuments%2FWorkplace%2FDev%2FResume&ga=1",
 };
